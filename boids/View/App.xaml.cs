10fe4650f006f1e2390e7bf1a732d9f77e212f24
Denis <Denis@DESKTOP-KBKV3K3>
2017-04-06 11:25:55 +0200

    added Service project for timer interfaces and service

--- boids/View/App.xaml.cs
+++ boids/View/App.xaml.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using ViewModel;
+using Microsoft.Practices.Unity;
+using Service;
 
 namespace View
 {
@@ -16,6 +18,9 @@ namespace View
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            var container = new UnityContainer();
+            container.RegisterType<ITimerService, TimerService>();
+
             base.OnStartup(e);
             var main = new MainWindow();
             main.DataContext = new SimulationViewModel();
