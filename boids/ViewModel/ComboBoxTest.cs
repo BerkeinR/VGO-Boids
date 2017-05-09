6c289b13da0834a1e4ce7c032042efa346eef654
Denis <Denis@DESKTOP-KBKV3K3>
2017-05-09 14:55:26 +0200

    started adding parameter sliders dynamically via itemscontrol

---
+++ boids/ViewModel/ComboBoxTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Reflection;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using Model.Species;
+using Bindings;
+
+namespace ViewModel
+{
+    public class ComboBoxTest : INotifyPropertyChanged
+    {
+        private SimulationViewModel simulationvm;
+        protected string m_FirstSelectedValue;
+
+        public string FirstSelectedValue
+        {
+            get { return m_FirstSelectedValue; }
+            set
+            {
+                Console.WriteLine(value);
+                if (m_FirstSelectedValue != value)
+                {
+                    m_FirstSelectedValue = value;
+                    UpdateParameterSliders(value);
+                    
+                    NotifyPropertyChanged("FirstSelectedValue");
+                }
+            }
+        }
+
+        protected ObservableCollection<BoidSpecies> m_FirstComboValues;
+
+        public ObservableCollection<BoidSpecies> FirstComboValues
+        {
+            get { return m_FirstComboValues; }
+            set
+            {
+                if (m_FirstComboValues != value)
+                {
+                    m_FirstComboValues = value;
+                    NotifyPropertyChanged("FirstComboValues");
+                }
+            }
+        }
+
+        public ObservableCollection<IParameter> parameters { get; set; }
+
+        public ComboBoxTest(SimulationViewModel simulationvm)
+        {
+            FirstComboValues = new ObservableCollection<BoidSpecies>(simulationvm.sim.Species);
+            this.simulationvm = simulationvm;
+            m_FirstComboValues = new ObservableCollection<BoidSpecies>(simulationvm.sim.Species);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            Console.WriteLine(propertyName);
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void UpdateParameterSliders(String type)
+        {
+            if(type.Equals("Model.Species.HunterSpecies"))
+            {
+                var bindings = simulationvm.sim.Species.First().Bindings;
+                var pars = bindings.Parameters;
+                
+                parameters = new ObservableCollection<IParameter>(pars);
+            }
+        }
+    }
+}
--- boids/ViewModel/SimulationViewModel.cs
+++ boids/ViewModel/SimulationViewModel.cs
@@ -21,6 +21,7 @@ namespace ViewModel
 
         public ICommand AddBoid { get; set; }
         public Cell<double> speedCell { get; set; }
+        public ComboBoxTest boxTest { get; set; }
 
         public SimulationViewModel()
         {
@@ -28,6 +29,7 @@ namespace ViewModel
 
             AddBoid = new AddBoid(this);
             speedCell = Cell.Create(100.0);
+            boxTest = new ComboBoxTest(this);
 
             this.sim.Species[0].CreateBoid(new Vector2D(50, 50));
             this.sim.Species[1].CreateBoid(new Vector2D(150, 150));
@@ -36,12 +38,12 @@ namespace ViewModel
             timer.Tick += Timer_Tick;
             timer.Start(new TimeSpan(0, 0, 0, 0, 15));
 
-            var bindings = sim.Species.First().Bindings;
-            var pars = bindings.Parameters;
-            var speed  = pars.Where(c => c.Id == "MAximum speed").Single();
-            var rangedDoubleSpeed = speed as RangedDoubleParameter;
-            var value = bindings.Read(rangedDoubleSpeed);
-            value.Value = speedCell.Value;
+          //var bindings = sim.Species.First().Bindings;
+            //var pars = bindings.Parameters;
+            //var speed  = pars.Where(c => c.Id == "Maximum speed").Single();
+            //var rangedDoubleSpeed = speed as RangedDoubleParameter;
+            //var value = bindings.Read(rangedDoubleSpeed);
+            //value.Value = speedCell.Value;
         }
 
         private void Timer_Tick(ITimerService obj)
